83931a72a1005f0a4ffd6dd4d8890ab0ec9c59fe
Lior Fogel <liorfo57@gmail.com>
2023-01-08 01:12:23 +0200

    added reconnect property

--- main.py
+++ main.py
@@ -2,15 +2,8 @@ import sqlite3
 import os
 import nextcord
 from nextcord.ext import commands
-from dotenv import load_dotenv
 import asyncio
 import config
-
-async def start(client):
-        for folder in os.listdir("modules"):
-            if os.path.exists(os.path.join("modules", folder, "cog.py")):
-                client.load_extension(f"modules.{folder}.cog")
-        await client.start(config.BOT_TOKEN)
 
 def main():
     client = commands.Bot(intents=nextcord.Intents.all(), command_prefix=config.PREFIX, case_insensitive=True)
@@ -24,7 +17,10 @@ def main():
             if "hello" in message.content.lower():
                 await message.channel.send(f"Hey {message.author.mention} this my first time here")
             await client.process_commands(message)
-    asyncio.run(start(client))
+    for folder in os.listdir("modules"):
+        if os.path.exists(os.path.join("modules", folder, "cog.py")):
+            client.load_extension(f"modules.{folder}.cog")
+    client.run(config.BOT_TOKEN, reconnect=True)
 
 if __name__ == "__main__":
     main()
