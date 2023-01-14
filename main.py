c45eef573f171b858cd7d868cd6d29bb27d8566f
Lior Fogel <liorfo57@gmail.com>
2023-01-14 17:55:05 +0200

    disconnect

--- main.py
+++ main.py
@@ -5,20 +5,27 @@ import config
 
 def main():
     client = commands.Bot(intents=nextcord.Intents.all(), command_prefix=config.PREFIX, case_insensitive=True)
-
+    client._help_command = None
     @client.event
     async def on_ready():
         if client:
             print(f"{client.user.name} has connected to discord")
 
-
+    
     @client.event
     async def on_message(message):
         if client.user.id != message.author.id:
             if "hello chtholly" in message.content.lower():
                 await message.channel.send(f"Hey {message.author.mention} <:love:1063551085660344501>")
+            if 'c' in message.content[0].lower():
+                message.content = 'c' + message.content[1:]
             await client.process_commands(message)
     
+    @client.command()
+    @commands.is_owner()
+    async def shutdown(ctx):
+        print("shutdown")
+        exit(0)
 
     #loading cogs
     for folder in os.listdir("modules"):
