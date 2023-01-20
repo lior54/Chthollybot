b6daa8a9dfb2327a75389621862e9fcce8c2cbdc
Lior Fogel <liorfo57@gmail.com>
2023-01-20 12:06:53 +0200

    tag sorter

---
+++ modules/karuta_tag_sorter/cog.py
@@ -0,0 +1,28 @@
+import os
+import nextcord
+from nextcord.ext import commands
+import modules.karuta_tag_sorter.sorter
+
+class Karuta(commands.Cog, name="Karuta"):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @nextcord.slash_command(name="karuta_sorter", description="Recommending for sort based on ksheet, each series to be tagged must have at least one card tagged")
+    async def karuta_sorter(self, interaction: nextcord.Interaction, link: str = nextcord.SlashOption(required=True, name="link", description="link to ksheet file"), exclude: str = nextcord.SlashOption(required=True, name="exclude", description="tags to exclude from tag suggestions saperated by ,")):
+        if not link.startswith("https://") and link.endswith(".csv"):
+            await interaction.response.send_message("invalid link")
+        else:
+            exclude = exclude.replace(" ", "")
+            if "," in exclude:
+                exclude = exclude.split(",")
+            else:
+                exclude = [].append(exclude)
+            exclude.append("")
+            data = modules.karuta_tag_sorter.sorter.karuta_sorter(link=link, exclude=exclude)
+            with open(f"{interaction.user.id}.txt", "w", encoding="utf-8") as file:
+                file.write(data)
+            await interaction.response.send_message(file=nextcord.File(fp=f"{interaction.user.id}.txt"))
+            os.remove(f"{interaction.user.id}.txt")
+
+def setup(bot: commands.Bot):
+    bot.add_cog(Karuta(bot))
---
+++ modules/karuta_tag_sorter/sorter.py
@@ -0,0 +1,51 @@
+import csv
+import urllib.request
+
+
+def sort_order(elem):
+    return elem["series"], elem["character"], -int(elem["edition"]), -int(elem["quality"])
+
+def karuta_sorter(link:str, exclude:list):
+    data = urllib.request.urlopen(f"{link}")
+    data = [l.decode("utf-8") for l in data.readlines()]
+    cr = csv.DictReader(data)
+    sort_card = {}
+    tags = {}
+    for row in cr:
+        if row["tag"] in ["sort", ""]:
+            if row["series"] not in sort_card:
+                sort_card[row["series"]] = [row["code"]]
+            else:
+                sort_card[row["series"]].append(row["code"])
+        elif not(row["tag"] in exclude or row["tag"] in tags):
+            tags[row["series"]] = row["tag"]
+                
+    unknown = {}
+    count = 0
+
+    #sort dicts
+    temp = list(tags.keys())
+    temp.sort()
+    tags = {i: tags[i] for i in temp}
+    temp = list(sort_card.keys())
+    temp.sort()
+    sort_card = {i: sort_card[i] for i in temp}
+
+    #making final list
+    final = {}
+    for series in sort_card:
+        if series not in tags:
+            unknown[series] = sort_card[series]
+        elif tags[series] in final:
+            final[tags[series]] += sort_card[series]
+        else:
+            final[tags[series]] = sort_card[series]
+
+    #formatting output message
+    send = ""
+    for key in final:
+        send += f'kt {key} {", ".join(final[key])}\n\n'
+    send += "\n\nunknown cards to tag:\n"
+    for key in unknown:
+        send += f"{key}: {', '.join(unknown[key])}\n"
+    return send
